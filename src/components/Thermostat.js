cb57a68290db2e09e457b89bc6472ae4c96bc22d
BennEntterprise <kyle@bennentterprise.com>
2019-11-21 19:32:20 -0500

    change thermostat to fc

--- src/components/Thermostat.js
+++ src/components/Thermostat.js
@@ -1,21 +1,17 @@
 import React from 'react';
 
-class Thermostat extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render(){
-      return(
-        <div>
-          <div id="thermostat">
-            <div id="temperature" style={{width: this.props.temp +"%"}}>
-              {this.props.temp}
-            </div>
-          </div>
+const Thermostat = (props) => {
+ 
+  return(
+    <div>
+      <div id="thermostat">
+        <div id="temperature" style={{width: props.temp +"%"}}>
+          {props.temp}
         </div>
-      );
-  }
+      </div>
+    </div>
+  );
+  
 }
 
 export default Thermostat;
